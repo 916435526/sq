cb2831f38517617350330cba1082e2ecea6acba3
916435526 <916435526@qq.com>
2014-08-13 16:55:27 +0800

    蘑菇街登录完成
    解决了选项卡的相关问题，并且完成了网页底部的制作

--- mogujie/js/mugu.js
+++ mogujie/js/mugu.js
@@ -3,17 +3,17 @@ $(function(){
 	$("#login_top_left").click(function(){
 		$("#content_left").show()
 		$("#content_right").hide()
-		$("#login_top_right").css("color","red")
-		$("#login_top_left").css("color","yellow")
-		$("#login_top_right").css("border-bottom","solid #dcdcdc")
+		$("#login_top_left").css("color","#333333")
+		$("#login_top_right").css("color","#AAAAAA")
+		$("#login_top_left").removeClass("active");
+		$("#login_top_right").addClass("active");
 	})
 	$("#login_top_right").click(function(){
 		$("#content_left").hide()
 		$("#content_right").show()
-		$("#login_top_right").css("color","yellow")
-		$("#login_top_left").css("color","red")
-		$("#login_top_right").css("border-bottom","")
-		$("#login_top_left").css("border-bottom","1px solid #dcdcdc")
-
-	});
+		$("#login_top_left").css("color","#AAAAAA")
+		$("#login_top_right").css("color","#333333")
+		$("#login_top_right").removeClass("active")
+		$("#login_top_left").addClass("active")
+	})
 });
